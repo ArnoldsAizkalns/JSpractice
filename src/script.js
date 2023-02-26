52d7005b2a3d845fd636af1ac52118c102127ad5
Arnold <arnoldshaman@gmail.com>
2023-02-26 09:23:53 +0200

    36 commit

--- src/script.js
+++ src/script.js
@@ -1,41 +1,134 @@
 "use strict";
 
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        bg: 'red'
-    },
-    makeTest: function () {
-        console.log('Test');
+const arr = [1, 2, 3, 6, 8];
+
+/* arr.pop(); */
+arr.push(10);
+
+/* console.log(arr); */
+
+/* for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+
+} */
+
+for (let value of arr) {
+    console.log(value);
+}
+
+
+let a = 5,
+    b = a;
+
+b = b + 5;
+
+console.log(b);
+console.log(a);
+
+const obj = {
+    a: 5,
+    b: 1
+};
+
+const copy = obj;
+
+copy.a = 10;
+
+console.log(copy);
+console.log(obj);
+
+
+function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+    return objCopy;
+}
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
     }
 };
 
-options.makeTest();
+const newNumbers = copy(numbers);
 
-const { border, bg } = options.colors;
-
-/* console.log(Object.keys(options).length); */
-
-/* console.log(options.name); */   /* внимание */
-
-/* delete options.name;
-
-console.log(options); */
+newNumbers.a = 10;
+newNumbers.c.x = 10;
+/* console.log(newNumbers);
+console.log(numbers); */
 
 
-let counter = 0;
-for (let key in options) {
-    if (typeof (options[key]) === 'object') {
-        for (let i in options[key]) {
-            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-            counter++;
-        }
-    } else {
-        console.log(`Свойство ${key} имеет значение ${options[key]}`);
-        counter++;
-    }
+const add = {
+    d: 17,
+    e: 20
+};
+
+/* console.log(Object.assign(numbers, add)); */
+
+const clone = Object.assign({}, add);
+
+clone.d = 20;
+/* 
+console.log(add);
+console.log(clone); */
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'adasdasd';
+
+console.log(newArray);
+console.log(oldArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+    blogs = ['wordpress', 'livejournal', 'blogger'],
+    internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
 }
-console.log(counter);
+
+const num = [2, 5, 7];
+
+log(...num);
+
+
+const array = ['a', 'b'];
+const newAarray = [...array];
+
+const q = {
+    one: 1,
+    two: 2
+};
+
+const newObj = {
+    ...q
+};
+
+console.log(q);
+
+const soldier = {
+    health: 400,
+    armor: 100
+};
+
+const john = {
+    health: 100
+};
+
+/* john.__proto__ = soldier; */
+Object.setPrototypeOf(john, soldier);
+
+/* console.log(john.health); */
+john.sayHello();
+
