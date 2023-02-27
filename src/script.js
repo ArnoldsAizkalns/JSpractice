e834e57a49e69a996f713134c7f769555e1e811c
Arnold <arnoldshaman@gmail.com>
2023-02-27 21:10:48 +0200

    38 commit

--- src/script.js
+++ src/script.js
@@ -1,134 +1,83 @@
 "use strict";
 
-const arr = [1, 2, 3, 6, 8];
+"use strict";
 
-/* arr.pop(); */
-arr.push(10);
+const personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+    start: function () {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-/* console.log(arr); */
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    rememberMyFilms: function () {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', ''),
+                b = prompt('На сколько оцените его?', '');
 
-/* for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function () {
+        if (personalMovieDB.count < 10) {
+            console.log("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log("Вы классический зритель");
+        } else if (personalMovieDB.count >= 30) {
+            console.log("Вы киноман");
+        } else {
+            console.log("Произошла ошибка");
+        }
+    },
+    showMyDB: function (hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: function () {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+    writeYourGenres: function () {
+        for (let i = 1; i < 2; i++) {
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
 
-} */
+            if (genre === '' || genre == null) {
+                console.log('Вы ввели некорректные данные или не ввели их вовсе');
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] = genre;
+            }
 
-for (let value of arr) {
-    console.log(value);
-}
+            // Альтернативный вариант из урока
 
+            // let genres = prompt(`Введите ваши любимые жанры через запятую`).toLowerCase();
 
-let a = 5,
-    b = a;
+            // if (genres === '' || genres == null) {
+            //     console.log('Вы ввели некорректные данные или не ввели их вовсе');
+            //     i--;
+            // } else {
+            //     personalMovieDB.genres = genres.split(', ');
+            //     personalMovieDB.genres.sort();
+            // } 
+        }
 
-b = b + 5;
-
-console.log(b);
-console.log(a);
-
-const obj = {
-    a: 5,
-    b: 1
-};
-
-const copy = obj;
-
-copy.a = 10;
-
-console.log(copy);
-console.log(obj);
-
-
-function copy(mainObj) {
-    let objCopy = {};
-
-    let key;
-    for (key in mainObj) {
-        objCopy[key] = mainObj[key];
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        });
     }
-    return objCopy;
-}
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
-    }
 };
-
-const newNumbers = copy(numbers);
-
-newNumbers.a = 10;
-newNumbers.c.x = 10;
-/* console.log(newNumbers);
-console.log(numbers); */
-
-
-const add = {
-    d: 17,
-    e: 20
-};
-
-/* console.log(Object.assign(numbers, add)); */
-
-const clone = Object.assign({}, add);
-
-clone.d = 20;
-/* 
-console.log(add);
-console.log(clone); */
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-
-newArray[1] = 'adasdasd';
-
-console.log(newArray);
-console.log(oldArray);
-
-const video = ['youtube', 'vimeo', 'rutube'],
-    blogs = ['wordpress', 'livejournal', 'blogger'],
-    internet = [...video, ...blogs, 'vk', 'facebook'];
-
-console.log(internet);
-
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-
-const num = [2, 5, 7];
-
-log(...num);
-
-
-const array = ['a', 'b'];
-const newAarray = [...array];
-
-const q = {
-    one: 1,
-    two: 2
-};
-
-const newObj = {
-    ...q
-};
-
-console.log(q);
-
-const soldier = {
-    health: 400,
-    armor: 100
-};
-
-const john = {
-    health: 100
-};
-
-/* john.__proto__ = soldier; */
-Object.setPrototypeOf(john, soldier);
-
-/* console.log(john.health); */
-john.sayHello();
-
